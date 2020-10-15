24a630ef5280d7116159119c07c582b446e312fc
FrancisGuedes <62439681+FrancisGuedes@users.noreply.github.com>
2020-10-15 23:46:11 +0100

    added HTML for 2 containers

--- src/components/pages/programPage.js
+++ src/components/pages/programPage.js
@@ -4,46 +4,205 @@ import React from 'react';
 function Program() {
   return (
     <div className="programPage" id='programNavbar_link'>
+      <img className='img_programPage' role='img' src='/images/pages/base/program/Programa background.jpg' alt='Programa' id='programNavbar_link' />
       <div className='programPage-container'>
-        <div className='programInner-container'>
-          
-          <img className='img_programPage' role='img' src='/images/pages/base/program/Programa background.jpg' alt='Programa' id='programNavbar_link' />
 
-          <div>
-            <section class="_headLineWrapper_wgmchy">
-              <div class="_imageContainer_1gudk09">
-                <img class="_image_1gn5ch6" src="" alt="profile"/>
-              </div>
-              <div class="_headLine_1qdszxi">
-                  <div class="_vxnubnv"> B-Girl </div>
-                  <div class="_12tmfqcb"> Luma </div>
-              </div>
-            </section>
-            <div class="_paragraph_56umsx winner-card-body__paragraph">Da Colômbia, a B-Girl Luma dança desde a sua infância e iniciou-se no Breaking em 2010. Tem viajado pelo mundo e conectado com bailarinos de diferentes culturas e com o mesmo foco.
-            </div>
-          </div>
+        <div className='programLayout-container_0'>
 
-          <div>
-            <section class="_headLineWrapper_wgmchy">
-              <div class="_imageContainer_1gudk09">
-                <img class="_image_1gn5ch6" src="" alt="profile" />
+          <div class="innerContainer_0">
+            <div className='innerContainer_header'>
+              <div className='innerContainer_header-title'>
+                <h2>19 DE DEZEMBRO</h2>
+                  <div className='innerContainer_header-titleParagraph'> 
+                    <h5>10€/Workshop | 1h duração</h5>
+                  </div>
               </div>
-              <div class="_headLine_1qdszxi">
-                  <div class="_vxnubnv"> B-Girl </div>
-                  <div class="_12tmfqcb"> Luma </div>
-              </div>
-            </section>
-            <div class="_paragraph_56umsx winner-card-body__paragraph">Da Colômbia, a B-Girl Luma dança desde a sua infância e iniciou-se no Breaking em 2010. Tem viajado pelo mundo e conectado com bailarinos de diferentes culturas e com o mesmo foco.
             </div>
+
+            <div className='innerContainer_line'></div>
+
+            <div class="innerContainer_body-Paragraph">
+
+              <div class="inline-paragraph_Container">
+                <div className="inline-paragraph_textContainer">
+                  <span className='inline-paragraph_text'>Workshop de Top Styles</span>
+                </div>
+              
+                <div className='inline-paragraph_timeContainer'>
+                  <span className='inline-paragraph_time'>10:00</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="inline-paragraph_Container">
+              <div className="inline-paragraph_textContainer">
+                <span className='inline-paragraph_text'>Workshop de Breaking</span>
+              
+                <div className='inline-paragraph_timeContainer'>
+                  <span className='inline-paragraph_time'>11:30</span>
+                </div>
+              </div>
+            
+             </div>
+
+            <div class="inline-paragraph_Container">
+              <div className="inline-paragraph_textContainer">
+                <span className='inline-paragraph_text'>Workshop de Graffiti</span>
+              
+                <div className='inline-paragraph_timeContainer'>
+                  <span className='inline-paragraph_time'>14:00</span>
+                </div>
+              </div>
+            
+            </div>
+
+            <div class="inline-paragraph_Container">
+              <div className="inline-paragraph_textContainer">
+                <span className='inline-paragraph_text'>Workshop de Beatbox</span>
+              
+                <div className='inline-paragraph_timeContainer'>
+                  <span className='inline-paragraph_time'>16:30</span>
+                </div>
+              </div>
+            
+            </div>
+
+            <div class="inline-paragraph_Container">
+              <div className="inline-paragraph_textContainer">
+                <span className='inline-paragraph_text'>Workshop de Djing/Produção</span>
+              
+                <div className='inline-paragraph_timeContainer'>
+                  <span className='inline-paragraph_time'>17:00</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="inline-paragraph_Container">
+              <div className="inline-paragraph_textContainer">
+                <span className='inline-paragraph_text'>Workshop de Mcing/Rap</span>
+              
+                <div className='inline-paragraph_timeContainer'>
+                  <span className='inline-paragraph_time'>18:30</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="inline-paragraph_Container">
+              <div className="inline-paragraph_textContainer">
+                <span className='inline-paragraph_text'>Debate DLàC - gratuito</span>
+              
+                <div className='inline-paragraph_timeContainer'>
+                  <span className='inline-paragraph_time'>21:00</span>
+                </div>
+              </div>
+            </div>
+              
+              {/*BUTÃO*/}
           </div>
+        </div>
+
+        <div className='programLayout-container_1'>
+        <div class="innerContainer_0">
+            <div className='innerContainer_header'>
+              <div className='innerContainer_header-title'>
+                <h2>20 DE DEZEMBRO</h2>
+                  <div className='innerContainer_header-titleParagraph'> 
+                    <h5>Semi Finais e Finais</h5>
+                  </div>
+              </div>
+            </div>
+
+            <div className='innerContainer_line'></div>
+
+            <div class="innerContainer_body-Paragraph">
+
+              <div class="inline-paragraph_Container">
+                <div className="inline-paragraph_textContainer">
+                  <span className='inline-paragraph_text'>Competição de Top Styles</span>
+                </div>
+              
+                <div className='inline-paragraph_timeContainer'>
+                  <span className='inline-paragraph_time'>14:00</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="inline-paragraph_Container">
+              <div className="inline-paragraph_textContainer">
+                <span className='inline-paragraph_text'>Competição de Breaking</span>
+              
+                <div className='inline-paragraph_timeContainer'>
+                  <span className='inline-paragraph_time'>15:00</span>
+                </div>
+              </div>
+            
+             </div>
+
+            <div class="inline-paragraph_Container">
+              <div className="inline-paragraph_textContainer">
+                <span className='inline-paragraph_text'>Competição de Graffiti</span>
+              
+                <div className='inline-paragraph_timeContainer'>
+                  <span className='inline-paragraph_time'>16:00</span>
+                </div>
+              </div>
+            
+            </div>
+
+            <div class="inline-paragraph_Container">
+              <div className="inline-paragraph_textContainer">
+                <span className='inline-paragraph_text'>Competição de Beatbox</span>
+              
+                <div className='inline-paragraph_timeContainer'>
+                  <span className='inline-paragraph_time'>17:00</span>
+                </div>
+              </div>
+            
+            </div>
+
+            <div class="inline-paragraph_Container">
+              <div className="inline-paragraph_textContainer">
+                <span className='inline-paragraph_text'>Competição de Djing/Produção</span>
+              
+                <div className='inline-paragraph_timeContainer'>
+                  <span className='inline-paragraph_time'>18:00</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="inline-paragraph_Container">
+              <div className="inline-paragraph_textContainer">
+                <span className='inline-paragraph_text'>Competição de Mcing/Rap</span>
+              
+                <div className='inline-paragraph_timeContainer'>
+                  <span className='inline-paragraph_time'>19:00</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="inline-paragraph_Container">
+              <div className="inline-paragraph_textContainer">
+                <span className='inline-paragraph_text'>Entrega de Prémios</span>
+              
+                <div className='inline-paragraph_timeContainer'>
+                  <span className='inline-paragraph_time'>19:30</span>
+                </div>
+              </div>
+            </div>
+              
+              {/*BUTÃO*/}
+          </div>
+        </div>
 
 
      
 
-        </div>
+        
+    
       </div>
     </div>
-  )
-}
+    )
+  }
+
 
 export default Program;
