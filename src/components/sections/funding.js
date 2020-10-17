479b7d038be743dffbce8666ce9ac9dafa8ccf38
Inês Martins <in.afmartins@gmail.com>
2020-10-18 00:10:02 +0100

    fixes bug on funding

--- src/components/sections/funding.js
+++ src/components/sections/funding.js
@@ -7,7 +7,7 @@ function Funding() {
 			<div className="funding-container">
 				<div className="funding-left-col">
 					<div className="funding-left-col-header">
-						O goal da campanha são <span className="fundraising-value">1700$</span> para cobrir despesas com recursos humanos, materiais e técnicas.
+						O goal da campanha são <span className="fundraising-value">€1700</span> para cobrir despesas com recursos humanos, materiais e técnicas.
 						<br></br>
 						<i className="brevemente">Os dados de contribuição serão disponibilizados brevemente.</i>
 					</div>
