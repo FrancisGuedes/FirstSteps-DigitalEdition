98e2809053937e1a6701db69795cc0e5a1b1b3de
nunofaria11 <2145649+nunofaria11@users.noreply.github.com>
2020-10-18 00:36:17 +0100

    Add program subscription buttons

--- src/components/sections/program.js
+++ src/components/sections/program.js
@@ -5,11 +5,17 @@ import { Button } from "../headerComponent/button";
 function Program() {
 	return (
 		<div id="programa" className="program">
-			<div className="left_container">
+			<div className="program--container left_container">
 				<img src="/images/pages/base/program/19_Dezembro.png"/>
+				<a href="https://www.google.com" target="_blank" className="program--subscribe">
+					<img className="program--subscribeIcon" src="/images/pages/base/program/Programa - Inscreve-te.svg" />
+				</a>
 			</div>
-			<div className="right_container">
+			<div className="program--container right_container">
 				<img src="/images/pages/base/program/20_Dezembro.png"/>
+				<a href="https://www.google.com" target="_blank" className="program--subscribe">
+					<img className="program--subscribeIcon" src="/images/pages/base/program/Programa - Inscreve-te.svg" />
+				</a>
 			</div>
 		</div>
 	);
