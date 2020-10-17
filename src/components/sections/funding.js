27b36642f79f8cb6beaac37a218d7ab1d8d45c51
Inês Martins <in.afmartins@gmail.com>
2020-10-17 22:55:43 +0100

    adds to be added soon info

--- src/components/sections/funding.js
+++ src/components/sections/funding.js
@@ -8,6 +8,8 @@ function Funding() {
 				<div className="funding-left-col">
 					<div className="funding-left-col-header">
 						O goal da campanha são <span className="fundraising-value">1700$</span> para cobrir despesas com recursos humanos, materiais e técnicas.
+						<br></br>
+						<i className="brevemente">Os dados de contribuição serão disponibilizados brevemente.</i>
 					</div>
 					<div className="funding-left-col-body">
 						No entanto, este orçamento não contempla nenhum valor extra para a nossa associação, que reinveste anualmente os fundos recebidos na criação de mais eventos para a comunidade. Por isso, queremos reforçar aqui que quaisquer excedentes irão diretamente para o fundo da associação e consequentemente para o futuro e sustentabilidade do evento First Steps.
