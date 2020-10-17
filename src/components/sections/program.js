d283eb422c4a571f8ca8236f0c948306587dd931
Inês Martins <in.afmartins@gmail.com>
2020-10-17 23:22:37 +0100

    finishes program section

--- src/components/sections/program.js
+++ src/components/sections/program.js
@@ -6,10 +6,10 @@ function Program() {
 	return (
 		<div id="programa" className="program">
 			<div className="left_container">
-				<img src="/images/pages/base/program/19_Dezembro.svg"/>
+				<img src="/images/pages/base/program/19_Dezembro.png"/>
 			</div>
 			<div className="right_container">
-				<img src="/images/pages/base/program/20_Dezembro.svg"/>
+				<img src="/images/pages/base/program/20_Dezembro.png"/>
 			</div>
 		</div>
 	);
