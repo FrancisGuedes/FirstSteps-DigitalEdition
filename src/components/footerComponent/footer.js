023eeba7986a434e0310f0cbd86abff891404c95
FrancisGuedes <62439681+FrancisGuedes@users.noreply.github.com>
2020-10-20 23:30:43 +0100

    finished footer

--- src/components/footerComponent/footer.js
+++ src/components/footerComponent/footer.js
@@ -13,36 +13,41 @@ class Footer extends Component {
 			<footer>
 				<div className="footer-wrapper">						<div className="container-left">
 						<span className="container-text">Os nossos parceiros</span>
-						<img
-							className="img_partner"
-							src="/images/footer/parceiros-46.png"
-							alt="Rimas e Batidas"
-							href=""
-						/>
-						<img
-							className="img_partner"
-							src="/images/footer/parceiros-49.png"
-							alt="Portugal Beatbox League"
-							href=""
-						/>
-						<img
-							className="img_partner"
-							src="/images/footer/parceiros-48.png"
-							alt="Dance Planet"
-							href=""
-						/>
-						<img
-							className="img_partner"
-							src="/images/footer/parceiros-50.png"
-							alt="Loop"
-							href=""
-						/>
-						<img
-							className="img_partner"
-							src="/images/footer/parceiros-47.png"
-							alt="Triangular 3D"
-							href=""
-						/>
+							<a href='https://www.rimasebatidas.pt/' target='_blank'>	
+								<img
+									className="img_partner"
+									src="/images/footer/parceiros-46.png"
+									alt="Rimas e Batidas"
+								/>
+							</a>
+							<a href='https://www.instagram.com/portugalbeatboxleague/?hl=pt' target='_blank'>
+								<img
+									className="img_partner"
+									src="/images/footer/parceiros-49.png"
+									alt="Portugal Beatbox League"
+								/>
+							</a>
+							<a href='https://www.danceplanet.pt/' target='_blank'>
+								<img
+									className="img_partner"
+									src="/images/footer/parceiros-48.png"
+									alt="Dance Planet"
+								/>
+							</a>
+							<a href='https://www.instagram.com/loop.festportugal/?hl=pt' target='_blank'>
+								<img
+									className="img_partner" 
+									src="/images/footer/parceiros-50.png"
+									alt="Loop"
+								/>
+							</a>
+							<a href='https://www.instagram.com/triangular3d/?hl=pt' target='_blank'>
+								<img
+									className="img_partner"
+									src="/images/footer/parceiros-47.png"
+									alt="Triangular 3D"
+								/>
+							</a>
 					</div>
 
 					<div
