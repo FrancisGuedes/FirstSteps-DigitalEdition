6dd8416aabf9e297504d6036c3cbdd7d932340f7
nunofaria11 <2145649+nunofaria11@users.noreply.github.com>
2020-10-25 13:20:14 +0000

    Fixes program scheduling position problems
    - created dedicated components for each day of the program
    - used dedicated button component with customized style to look like the
    provided svg

---
+++ src/components/sections/programScheduleData.js
@@ -0,0 +1,80 @@
+module.exports = [
+    {
+        "title": "19 de Dezembro",
+        "subTitle": "10 €/Workshop | 1h duração",
+        "url": "/?test=1",
+        "events": [
+            {
+                "date": "10:00",
+                "name": "Workshop de Top Styles"
+            },
+            {
+                "date": "11:30",
+                "name": "Workshop de Breaking"
+            },
+            {
+                "date": "14:00",
+                "name": "Workshop de Graffiti"
+            },
+            {
+                "date": "15:30",
+                "name": "Workshop de Beatbox"
+            },
+            {
+                "date": "17:00",
+                "name": "Workshop de Djing/Produção"
+            },
+            {
+                "date": "18:30",
+                "name": "Workshop de Mchg/Rap"
+            },
+            {
+                "date": "",
+                "name": ""
+            },
+            {
+                "date": "21:00",
+                "name": "Debate DLàC - gratuito"
+            }
+        ]
+    },
+    {
+        "title": "20 de Dezembro",
+        "subTitle": "Semi-finais e Finais",
+        "url": "/?test=2",
+        "events": [
+            {
+                "date": "14:00",
+                "name": "Competição de Top Styles"
+            },
+            {
+                "date": "15:00",
+                "name": "Competição de Breaking"
+            },
+            {
+                "date": "16:00",
+                "name": "Competição de Graffiti"
+            },
+            {
+                "date": "17:00",
+                "name": "Competição de Beatbox"
+            },
+            {
+                "date": "18:00",
+                "name": "Competição de Djing/Produção"
+            },
+            {
+                "date": "19:00",
+                "name": "Competição de Mchg/Rap"
+            },
+            {
+                "date": "",
+                "name": ""
+            },
+            {
+                "date": "19:30",
+                "name": "Entrega de Prémios"
+            }
+        ]
+    }
+]
